b7ba2262640c0cce4bc7ba135f22c8d2dfe07ad3
Muhammad Fajar Alfath <mfalfath25@gmail.com>
2022-05-28 23:35:02 +0700

    chore: grouping util functions

---
+++ client/src/utils/index.js
@@ -0,0 +1,6 @@
+import { formatDate } from './formatDate'
+import { getYear } from './getYear'
+import { timedLogout } from './timedLogout'
+import { toRupiah } from './toRupiah'
+
+export { formatDate, getYear, timedLogout, toRupiah }
